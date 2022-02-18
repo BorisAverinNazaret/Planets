f90dd49978ff23c4da773f3a05f2f9ebd7f7d802
Boris <averin500@yandex.ru>
2022-02-18 14:02:52 +0700

    перед заливкой корабля домой

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -12,16 +12,16 @@ public class Main : MonoBehaviour
     void Start()
     {
         // солнце 695500
-        // 0.387  149597870   57894
-        Planet.Generation("P000",    ae/695500, 0f, 0f, 0f);
-        Planet.Generation("P100",       3f,  0f, 0f, aeg * 0.387f);
-        Planet.Generation("P200",    6.05f,  0f, 0f, aeg * 0.723f);
-        Planet.Generation("P300",     6.4f,  0f, 0f, aeg);
-        Planet.Generation("P400",     3.4f,  0f, 0f, aeg * 1.524f);
-        Planet.Generation("P500",    70.8f,  0f, 0f, aeg * 5.203f);
-        Planet.Generation("P500",    60.1f,  0f, 0f, aeg * 9.539f);
-        Planet.Generation("P500",   25.35f,  0f, 0f, aeg * 19.19f);
-        Planet.Generation("P500",    24.9f,  0f, 0f, aeg * 30.06f);
+        //     
+        Planet.Generation("P000", 695f, 0f, 0f, 0f);
+        Planet.Generation("P100",           3f,  0f, 0f, aeg * 0.387f);
+        Planet.Generation("P200",        6.05f,  0f, 0f, aeg * 0.723f);
+        Planet.Generation("P300",       6.371f,  0f, 0f, aeg);
+        Planet.Generation("P400",         3.4f,  0f, 0f, aeg * 1.524f);
+        Planet.Generation("P500",        70.8f,  0f, 0f, aeg * 5.203f);
+        Planet.Generation("P600",        60.1f,  0f, 0f, aeg * 9.539f);
+        Planet.Generation("P700",       25.35f,  0f, 0f, aeg * 19.19f);
+        Planet.Generation("P800",        24.9f,  0f, 0f, aeg * 30.06f);
         //   Planet.Generation("Z", 10f, new Vector3(5, 0, 5));
         //   Debug.Log((4.87/2)+ (4.87%2));
 
